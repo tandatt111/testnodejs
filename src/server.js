d24e9bb87cc8aba2749747da9eb562d959795718
tandatt111 <tandatt111@gmail.com>
2023-06-19 23:23:57 +0700

    lan5

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const configViewEngine = require("./config/viewEngine");
-require("dotenv").config();
 const webRouter = require("./routers/web");
-const mysql = require("mysql2");
+const connection = require("./config/database");
 
 const app = express();
 const port = process.env.PORT || 8888;
@@ -15,17 +15,9 @@ configViewEngine(app);
 
 app.use("/", webRouter);
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  database: "tandatt",
-  port: 3307,
-  password: "123456",
-});
-
 connection.query("SELECT *FROM Users u", function (err, results, fields) {
   console.log("results", results); // results contains rows returned by server
-  console.log("fields", fields); // fields contains extra meta data about results, if available
+  ///console.log("fields", fields); // fields contains extra meta data about results, if available
 });
 
 app.listen(port, hostname, () => {
